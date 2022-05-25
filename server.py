891ec2a32b783fd2b8914e7563babef9b85a89a4
gerrit-schramm <75485842+gerrit-schramm@users.noreply.github.com>
2022-05-25 14:34:37 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,49 @@
+import time
+from paho.mqtt import client as mqtt_client
+from pylsl import StreamInlet, resolve_stream
+
+broker = 'localhost'
+port = 1883
+topic = "test/python/mqtt"
+client_id = f'python-mqtt-client'
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    #client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def publish(client):
+     msg_count = 0
+     while True:
+         time.sleep(1)
+         msg = f"messages: {msg_count}"
+         result = client.publish(topic, msg)
+         # result: [0, 1]
+         status = result[0]
+         if status == 0:
+             print(f"Send `{msg}` to topic `{topic}`")
+         else:
+             print(f"Failed to send message to topic {topic}")
+         msg_count += 1
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+    client.subscribe(topic)
+    client.on_message = on_message
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+
+if __name__ == '__main__':
+    run()
